4b5e3f4308701421deac5fa9fc0170f0e5f93acc
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-07 09:33:16 -0800

    solved mean

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 def calculate(list):
-    
+    # 1.) convert the list into Numpy array
+    arr = np.array(list).reshape((3, 3))
+    # 2.) calculate 
+    calculations ={
+        "mean": [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(list)],
+        "variance": [],
+        "standard deviation": [],
+        "max": [],
+        "min": [],
+        "sum": []
+    }
 
-
-
+    # 3.) Return
     return calculations
