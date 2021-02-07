c37b844b5b0a458fce5f25029bcbe2d1563647b6
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-07 10:38:58 -0800

    done

--- mean_var_std.py
+++ mean_var_std.py
@@ -12,8 +12,8 @@ def calculate(list):
         "mean": [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(list)],
         "variance": [np.var(arr, axis=0).tolist(), np.var(arr, axis=1).tolist(), np.var(list)],
         "standard deviation": [np.std(arr, axis=0).tolist(), np.std(arr, axis=1).tolist(), np.std(list)],
-        "max": [np.maximum(arr, axis=0).tolist(), np.maximum(arr, axis=1).tolist(), np.maximum(list)],
-        "min": [np.minimum(arr, axis=0).tolist(), np.minimum(arr, axis=1).tolist(), np.minimum(list)],
+        "max": [np.max(arr, axis=0).tolist(), np.max(arr, axis=1).tolist(), np.max(list)],
+        "min": [np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(list)],
         "sum": [np.sum(arr, axis=0).tolist(), np.sum(arr, axis=1).tolist(), np.sum(list)]
     }
 
